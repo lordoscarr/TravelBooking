f0b216a1e4391fc3acca483cadf3d8eb9cd08ede
lordoscar <paroscarandersson@gmail.com>
2018-11-07 12:17:46 +0100

    register in database works

--- app/src/main/java/com/lordoscar/travelbooking/Database.java
+++ app/src/main/java/com/lordoscar/travelbooking/Database.java
@@ -1,11 +1,14 @@
 package com.lordoscar.travelbooking;
 
+import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class Database extends Thread{
 
@@ -20,15 +23,78 @@ public class Database extends Thread{
             connection = DriverManager.getConnection("jdbc:postgresql://pgserver.mah.se/" + SQL_NAME +"?user=" + SQL_USER + "&password=" + SQL_PASSWORD);
             Log.d("Connection", connection.getCatalog());
 
-            PreparedStatement stmt = connection.
-                    prepareStatement("select * from dog");
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()){
-                Log.d("Connection", rs.getString("name") + ", " + rs.getString("breed") + ", " + rs.getString("birthyear"));
-            }
-
         }catch (Exception ex){
+            ex.printStackTrace();
             connection = null;
+        }
+    }
+
+    public ArrayList<String> getDrivers(){
+        try {
+            return new GetDrivers().execute().get();
+        }catch (Exception ex){
+            ex.printStackTrace();
+            return new ArrayList<String>();
+        }
+    }
+
+    public int registerTraveler(String name, String address, String email, String phone){
+        try {
+            return new RegisterUser().execute(name, address, email, phone).get();
+        }catch (Exception ex){
+            return -1;
+        }
+    }
+
+    private class RegisterUser extends AsyncTask<String, Void, Integer> {
+        @Override
+        protected Integer doInBackground(String... params) {
+            int id = -1;
+            String name = params[0];
+            String address = params[1];
+            String email = params[2];
+            String phone = params[3];
+
+            try{
+                PreparedStatement stmt = connection.
+                        prepareStatement("insert into traveler (name, address, email, phone) values ('" + name + "', '"+ address + "', '" + email + "', '" + phone + "');");
+
+                stmt.executeUpdate();
+
+                stmt = connection.
+                        prepareStatement("select * from traveler where (email = '" + email + "');");
+                ResultSet rs = stmt.executeQuery();
+
+                if(rs.next()){
+                    Log.d("RETRIEVED NEW USER", "ID: " + rs.getString("id") + ", Name: " + rs.getString("name"));
+                    id = Integer.parseInt(rs.getString("id"));
+                }
+
+            }catch (Exception ex){
+                if (ex.getMessage().contains("duplicate") && ex.getMessage().contains("unique_email")){
+                    id = -1;
+                }
+                ex.printStackTrace();
+            }
+            return id;
+        }
+    }
+
+    private class GetDrivers extends AsyncTask<String, Void, ArrayList<String>> {
+        @Override
+        protected ArrayList<String> doInBackground(String... params) {
+            ArrayList<String> drivers = new ArrayList<>();
+            try{
+                PreparedStatement stmt = connection.
+                        prepareStatement("select * from driver");
+                ResultSet rs = stmt.executeQuery();
+                while (rs.next()){
+                    drivers.add("Driver: " + rs.getString("name") + ", " + rs.getString("phone"));
+                }
+            }catch (Exception ex){
+                ex.printStackTrace();
+            }
+            return drivers;
         }
     }
 }
